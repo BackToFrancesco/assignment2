0a43bbe16c82c409a8a8ae88d868c21cbad14d19
Alessandro Cavaliere <ale.cava41@gmail.com>
2022-05-12 22:05:37 +0200

    initial commit

---
+++ src/test/java/it/unipd/mtss/AppTest.java
@@ -0,0 +1,26 @@
+package it.unipd.mtss;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+
+    @Test
+    public void TestFoo() {
+        App app = new App();
+        app.foo();
+    }
+}
