ac32616de7e2430c7a7ca8e4dc7489fa62a436de
Alessandro Cavaliere <ale.cava41@gmail.com>
2022-05-13 00:04:07 +0200

    ops, now closed #2

--- src/main/java/it/unipd/mtss/Bill.java
+++ src/main/java/it/unipd/mtss/Bill.java
@@ -1,7 +1,12 @@
+////////////////////////////////////////////////////////////////////
+// Alessandro Cavaliere 1224440
+// Filippo Brugnolaro 1217321
+////////////////////////////////////////////////////////////////////
 package it.unipd.mtss;
 
 import java.util.List;
 
 public interface Bill {
-    double getOrderPrice(List<EItem> itemsOrdered, User user) throws TotalAmountExceededException;
+    double getOrderPrice(List<EItem> itemsOrdered, User user)
+            throws TotalAmountExceededException;
 }
--- src/main/java/it/unipd/mtss/EShopBill.java
+++ src/main/java/it/unipd/mtss/EShopBill.java
@@ -1,10 +1,17 @@
+////////////////////////////////////////////////////////////////////
+// Alessandro Cavaliere 1224440
+// Filippo Brugnolaro 1217321
+////////////////////////////////////////////////////////////////////
 package it.unipd.mtss;
 
 import java.util.List;
 
 public class EShopBill implements Bill {
     @Override
-    public double getOrderPrice(List<EItem> itemsOrdered, User user) throws TotalAmountExceededException {
-        return itemsOrdered.stream().mapToDouble(eItem -> eItem.getPrice()).sum();
+    public double getOrderPrice(List<EItem> itemsOrdered, User user)
+            throws TotalAmountExceededException {
+        return itemsOrdered.stream()
+                .mapToDouble(eItem -> eItem.getPrice())
+                .sum();
     }
 }
